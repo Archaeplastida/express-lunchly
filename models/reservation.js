aed2252ee4ec6a5fc39fd8ec56814a120d862df0
Saif <saifisami2005@gmail.com>
2024-11-05 15:42:05 -0500

    Added method for saving reservation.

--- models/reservation.js
+++ models/reservation.js
@@ -87,6 +87,15 @@ class Reservation {
 
     return new Reservation(reservation);
   }
+
+  async save() {
+    if (this.id === undefined) {
+      const result = await db.query(`INSERT INTO reservations (customer_id, num_guests, start_at, notes) VALUES ($1, $2, $3, $4) RETURNING ID`, [this.customerId, this.numGuests, this.startAt, this.notes]);
+      this.id = results.rows[0].id;
+    } else {
+      await db.query(`UPDATE reservations SET num_guests=$1, start_at=$2, notes=$3 WHERE id =$4`, [this.numGuests, this.startAt, this.notes, this.id]);
+    }
+  }
 }
 
 
